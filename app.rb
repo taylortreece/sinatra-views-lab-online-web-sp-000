cfdd171d362808ecbd4502587ad9e8b2119e9f22
Taylor Treece <taylor.treece@protonmail.com>
2021-03-05 13:55:04 -0600

    commit tree 092dba7d50e6bf1bf02a93cfe63d122ed96a0984

--- app.rb
+++ app.rb
@@ -4,5 +4,29 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+	
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 
 end
+
+
+# App
+#   GET /hello
+#     sends a 200 status code (FAILED - 1)
+#     renders a template called "hello.erb" (FAILED - 2)
+#   GET /goodbye
+#     sends a 200 status code (FAILED - 3)
+#     renders a template called "goodbye.erb" (FAILED - 4)
+#   GET /date
+#     sends a 200 status code (FAILED - 5)
+#     renders a template called "date.erb" (FAILED - 6)
+#     includes the current date and time (FAILED - 7)
